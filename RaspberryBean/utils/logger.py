f3b8c4efbc6c2b79b56e8d07820f111841d04392
‎‧₊˚✧ piggly wiggly ✧˚₊‧ <matchadolly@gmail.com>
2025-05-16 06:08:20 +0100

    Update logger.py

--- RaspberryBean/utils/logger.py
+++ RaspberryBean/utils/logger.py
@@ -6,8 +6,8 @@ To use:
     log.info("Hello")
 🎀
 """
-
 import logging
+
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
