5026f5985c4147adb62f573f4d9716612f2246ed
‎‧₊˚✧ piggly wiggly ✧˚₊‧ <matchadolly@gmail.com>
2025-05-16 06:30:04 +0100

    Update display.py

---
+++ RaspberryBean/vision/display.py
@@ -0,0 +1,9 @@
+"""
+🎀
+Currently only logs calls. Display code will be added later.
+🎀
+"""
+from utils.logger import log
+
+def show_gif(path: str, loop_once: bool = False) -> None:
+    log.info("Would display GIF: %s (loop_once=%s)", path, loop_once)
