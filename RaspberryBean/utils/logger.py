22d5d22838bcdaf653a148235a8e90cb0b38a9c0
‎‧₊˚✧ piggly wiggly ✧˚₊‧ <matchadolly@gmail.com>
2025-05-16 06:00:58 +0100

    Update logger.py

---
+++ RaspberryBean/utils/logger.py
@@ -0,0 +1,13 @@
+"""
+🎀
+A centralised and colorised logger for Bean!
+To use:
+    from utils.logger import log
+    log.info("Hello")
+🎀
+"""
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+log = logging.getLogger("bean")
